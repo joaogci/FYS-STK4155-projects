07e6d092c21143faaa5f9b5b5d815181d5cef988
Jon <jonathan-kings@hotmail.com>
2021-09-15 09:55:41 +0200

    Add error functions

--- project_1/Regression.py
+++ project_1/Regression.py
@@ -5,3 +5,11 @@ class Regression():
     """
     ...
     
+    # --- Error functions ---
+
+    def R2(y_data, y_model):
+        return 1 - np.sum((y_data - y_model) ** 2) / np.sum((y_data - np.mean(y_data)) ** 2)
+    
+    def MSE(y_data,y_model):
+        n = np.size(y_model)
+        return np.sum((y_data-y_model)**2)/n
