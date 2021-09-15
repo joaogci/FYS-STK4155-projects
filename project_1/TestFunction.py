1a53b9c79baad5b32e6e50e47185042e79df8ed5
João Inácio <jgci2000@hotmail.com>
2021-09-15 13:50:08 +0200

    test function to test regression models

---
+++ project_1/TestFunction.py
@@ -0,0 +1,27 @@
+from Regression import Regression
+import numpy as np
+
+class Franke:
+    """
+        Test function class to test regression models
+        
+        Parameters: 
+            a (float): beginning of the interval for x and y
+            b (float): end of the interval for x and y
+            h (float): step between successive points
+    """
+        
+    def __init__(self, a, b, h):
+        self.x = (b - a) * np.random.rand(int((b - a) / h)) + a
+            
+    def data_set(self):        
+        self.y = np.exp(-self.x**2) + 1.5 * np.exp(-(self.x-2)**2)
+   
+    def add_noise(self):
+        self.y = self.y + np.random.normal(0, 0.5, self.x.shape)
+        
+    def initialize_regression(self):
+        return Regression(self.x, self.y)
+    
+
+
