38b073423ff669c79e57cae90aabdb76999898fa
Johan <jaf1@hotmail.no>
2021-09-22 13:18:32 +0200

    Add own train_test_split.

--- project_1/TrainTestSplitter.py
+++ project_1/TrainTestSplitter.py
@@ -17,12 +17,51 @@ class TrainTestSplitter(Splitter):
         self._test_size = test_size
 
     
-    def split(self, X: np.matrix, y: np.matrix) -> tuple:
+    def split_sk(self, X: np.matrix, y: np.matrix) -> tuple:
         """
             Splits the data into a training set and a test set
             Uses the SciKit Learn implementation (see https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html)
         """
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = self._test_size)
+        return {
+            'full': X,
+            'train': X_train,
+            'test': X_test
+        }, {
+            'full': y,
+            'train': y_train,
+            'test': y_test
+        }
+
+    def split(self, X: np.matrix, y: np.matrix) -> tuple:
+        """
+            Splits the design matrix and data into two sets of data; testing and training
+            Parameters:
+                split {float}: Between 0 and 1, the fraction of data to use for testing
+                seed {int}: Seed to use for the random generator
+            Returns:
+                {matrix, matrix, vector, vector} Split version of the data/design matrix
+        """
+        split = self._test_size
+        seed = 0
+        # Check inputs
+        assert X.shape[0] == y.shape[0], "SizeError: tt_split was given inputs of different sizes! Expects n_row(X) == len(y), given n_row(X) = {}, len(y) = {}!".format(X.shape[0], y.shape[0])
+            
+        # Init random number generator
+        rng = np.random.default_rng(seed=seed)
+
+        # Split the data into train and test sets
+        split_size = int(X.shape[0] * split)
+        
+        perm = rng.permuted(np.arange(0, X.shape[0]))
+        perm_X = X[perm, :]
+        perm_y = y[perm]
+        
+        X_train = perm_X[0:split_size, :]
+        y_train = perm_y[0:split_size]
+        X_test  = perm_X[split_size:, :]
+        y_test  = perm_y[split_size:]
+            
         return {
             'full': X,
             'train': X_train,
