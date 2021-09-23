1e838ce425a057339e92c641d73478b364f7f75f
João Inácio <jgci2000@hotmail.com>
2021-09-23 10:05:01 +0200

    test_ols tests line reg accuracy

--- project_1/test_ols.py
+++ project_1/test_ols.py
@@ -20,26 +20,7 @@ solver.set_splitter(TrainTestSplitter())
 solver.set_scaler(StandardScaler())
 
 solver.add_model(OLSModel())
-# solver.add_model(RidgeModel(5))
-solver.set_scaler(StandardScaler(with_std=False))
+solver.add_model(RidgeModel(5))
 
 solver.add_post_process(ErrDisplayPostProcess())
 solver.add_post_process(PlotPostProcess())
-
-
-solver1 = Solver(degree, seed=0)
-
-solver1.set_data_generator(ExponentialGenerator(degree=degree, count=100, min_x=-1, max_x=1, noise=0.1))
-
-solver1.set_splitter(TrainTestSplitter())
-
-solver1.add_model(OLSModel())
-# solver1.add_model(RidgeModel(5))
-
-solver1.add_post_process(ErrDisplayPostProcess())
-solver1.add_post_process(PlotPostProcess())
-
-solver.run()
-solver1.run()
-
-
