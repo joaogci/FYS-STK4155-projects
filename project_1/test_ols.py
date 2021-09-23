27d4080c1ed31a95525d04f6cc5c145f825b2942
João Inácio <jgci2000@hotmail.com>
2021-09-23 09:56:59 +0200

    test_ols for scaling vs no scaling

--- project_1/test_ols.py
+++ project_1/test_ols.py
@@ -7,19 +7,38 @@ from LassoModel import LassoModel
 from ErrDisplayPostProcess import ErrDisplayPostProcess
 from PlotPostProcess import PlotPostProcess
 from TrainTestSplitter import TrainTestSplitter
+from StandardScaler import StandardScaler
+from ExponentialGenerator import ExponentialGenerator
 
-degree = 5
+degree = 4
 
 solver = Solver(degree, seed=0)
 
-solver.set_data_generator(PolynomialGenerator(degree=degree, count=100, min_x=-15, max_x=20, noise=0.03))
+solver.set_data_generator(ExponentialGenerator(degree=degree, count=100, min_x=-1, max_x=1, noise=0.1))
 
 solver.set_splitter(TrainTestSplitter())
 
 solver.add_model(OLSModel())
-solver.add_model(RidgeModel(5))
+# solver.add_model(RidgeModel(5))
+solver.set_scaler(StandardScaler(with_std=False))
 
 solver.add_post_process(ErrDisplayPostProcess())
 solver.add_post_process(PlotPostProcess())
 
+
+solver1 = Solver(degree, seed=0)
+
+solver1.set_data_generator(ExponentialGenerator(degree=degree, count=100, min_x=-1, max_x=1, noise=0.1))
+
+solver1.set_splitter(TrainTestSplitter())
+
+solver1.add_model(OLSModel())
+# solver1.add_model(RidgeModel(5))
+
+solver1.add_post_process(ErrDisplayPostProcess())
+solver1.add_post_process(PlotPostProcess())
+
 solver.run()
+solver1.run()
+
+
