f0f0d41bcf71bf40fcd58a4c3d8a32aaaae8debd
João Inácio <jgci2000@hotmail.com>
2021-09-23 10:22:03 +0200

    updated test_ols

--- project_1/test_ols.py
+++ project_1/test_ols.py
@@ -17,10 +17,11 @@ solver = Solver(degree, seed=0)
 solver.set_data_generator(ExponentialGenerator(degree=degree, count=100, min_x=-1, max_x=1, noise=0.1))
 
 solver.set_splitter(TrainTestSplitter())
-solver.set_scaler(StandardScaler())
 
 solver.add_model(OLSModel())
 solver.add_model(RidgeModel(5))
 
 solver.add_post_process(ErrDisplayPostProcess())
 solver.add_post_process(PlotPostProcess())
+
+solver.run()
