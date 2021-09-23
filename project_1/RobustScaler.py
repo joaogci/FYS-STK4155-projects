16392e66e7d5a7a1d1e47d5e1963f20dc9c8e598
João Inácio <jgci2000@hotmail.com>
2021-09-23 10:21:50 +0200

    scalers working

--- project_1/RobustScaler.py
+++ project_1/RobustScaler.py
@@ -14,12 +14,16 @@ class RobustScaler(Scaler):
                 X (np.matrix): Principal design matrix
         """
         
-        self._median = np.zeros(X.shape[1] - 1)
-        self._inter_quartile_range = np.zeros(X.shape[1] - 1)
-        
-        for i in range(1, X.shape[1]):
-            self._median[i - 1] = np.median(X[:, i])
-            self._inter_quartile_range[i - 1] = np.percentile(X[:, i], 75) - np.percentile(X[:, i], 25)
+        if X.shape[1] == 1:
+            self._median = np.median(X[:, 0])
+            self._inter_quartile_range = np.percentile(X[:, 0], 75) - np.percentile(X[:, 0], 25) 
+        else:
+            self._median = np.zeros(X.shape[1] - 1)
+            self._inter_quartile_range = np.zeros(X.shape[1] - 1)
+            
+            for i in range(1, X.shape[1]):
+                self._median[i - 1] = np.median(X[:, i])
+                self._inter_quartile_range[i - 1] = np.percentile(X[:, i], 75) - np.percentile(X[:, i], 25)
 
     def scale(self, X: np.matrix) -> np.matrix:
         """
@@ -28,9 +32,11 @@ class RobustScaler(Scaler):
                 X (np.matrix): Design matrix to scale
         """
         
-        X_scaled = np.zeros(X.shape)
+        X_scaled = np.ones(X.shape)
         
+        if X.shape[1] == 1:
+            X_scaled[: ,0] = (X[:, 0] - self._median) / self._inter_quartile_range
         for i in range(1, X.shape[1]):
-            X_scaled[:, i] = (X[:, i] - self._median) / self._inter_quartile_range
+            X_scaled[:, i] = (X[:, i] - self._median[i - 1]) / self._inter_quartile_range[i - 1]
         
         return X_scaled
