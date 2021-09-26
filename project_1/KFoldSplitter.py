4cc5cc1a601db50fba24b0276d1bb1d6c36138a8
João Inácio <jgci2000@hotmail.com>
2021-09-26 15:32:58 +0200

    full KFold implementation

--- project_1/KFoldSplitter.py
+++ project_1/KFoldSplitter.py
@@ -17,26 +17,37 @@ class KFoldSplitter(Splitter):
         self._shuffle = shuffle
         self._seed = seed
 
-    def split(self, X: np.matrix, y: np.matrix) -> tuple:
+    def split(self, x: np.matrix, y: np.matrix) -> tuple:
         """
             Splits the design matrix and data into k sets
         """
         # Check inputs
-        assert X.shape[0] == y.shape[0], "SizeError: tt_split was given inputs of different sizes! Expects n_row(X) == len(y), given n_row(X) = {}, len(y) = {}!".format(X.shape[0], y.shape[0])
+        assert x.shape[0] == y.shape[0], "SizeError: KFold was given inputs of different sizes! Expects n_row(X) == len(y), given n_row(X) = {}, len(y) = {}!".format(x.shape[0], y.shape[0])
         
-        X_KFold = dict()
+        # Output dictionaries
+        x_KFold = dict()
         y_KFold = dict()
         
         # If there is a need to shuffle the data
         if self._shuffle:
-            # Init random number generator
             rng = np.random.default_rng(seed=self._seed)
             
-            perm = rng.permuted(np.arange(0, X.shape[0]))
-            X = X[perm, :]
+            perm = rng.permuted(np.arange(0, x.shape[0]))
+            x = x[perm]
             y = y[perm]
         
         # Split into k folds
+        KFold_size = np.floor(y.shape[0] / self._k)
+        
+        for k in range(self._k):
+            train_idx = np.concatenate((np.arange(0, k * KFold_size, dtype=int), np.arange((k+1) * KFold_size, self._k * KFold_size, dtype=int)))
+            test_idx = np.arange(k * KFold_size, (k+1) * KFold_size, dtype=int)
             
-        return X_KFold, y_KFold
+            x_KFold['train_' + str(k)] = x[train_idx]
+            x_KFold['test_' + str(k)] = x[test_idx]
+            
+            y_KFold['train_' + str(k)] = y[train_idx]
+            y_KFold['test_' + str(k)] = y[test_idx]
+
+        return x_KFold, y_KFold
     
