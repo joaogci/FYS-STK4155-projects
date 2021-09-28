e9122577c847041806fcc9ecc3810beaf9140c3a
João Inácio <jgci2000@hotmail.com>
2021-09-28 10:48:09 +0200

    exercise 1 done!

--- project_1/exercise_1.py
+++ project_1/exercise_1.py
@@ -1,25 +1,128 @@
+import numpy as np
+import numpy.linalg as linalg
 
-from Solver import Solver
-from OLSModel import OLSModel
-from FrankeGenerator import FrankeGenerator
-from TrainTestSplitter import TrainTestSplitter
-from PlotPostProcess import PlotPostProcess
-from ErrDisplayPostProcess import ErrDisplayPostProcess
-from StandardScaler import StandardScaler
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
+from sklearn.model_selection import train_test_split
+
+from functions import *
+
+# parameters
 degree = 5
-solver = Solver(degree=degree, fit_intercept=False)
+a = 0
+b = 1
+n = 10
+noise = 0.1
 
-solver.set_data_generator(FrankeGenerator(0, 1, 50, random=True, noise=0.1))
-solver.set_splitter(TrainTestSplitter())
-solver.add_model(OLSModel())
+# random number generator
+seed = 0
+rng = np.random.default_rng(np.random.MT19937(seed=seed))
 
-solver.add_post_process(ErrDisplayPostProcess())
-plotter = PlotPostProcess(show=False, title='Predictions - Unscaled')
-solver.add_post_process(plotter)
-solver.run()
+# generate x and y data
+# the generation can be randomly distributed
+x = np.linspace(a, b, n)
+y = np.linspace(a, b, n)
+# x = np.sort(rng.uniform(a, b, n))
+# y = np.sort(rng.uniform(a, b, n))
 
-solver.set_scaler(StandardScaler(with_std=False))
-plotter.show = True
-plotter.title = 'Predictions - Scaled'
-solver.run()
+# create a meshgrid and compute the franke function
+x, y = np.meshgrid(x, y)
+z = franke_function(x, y)
+
+# add noise to the data
+z += noise * rng.normal(0, 1, z.shape)
+
+# ravel the data 
+x_ravel = np.ravel(x).reshape((np.ravel(x).shape[0], 1))
+y_ravel = np.ravel(y).reshape((np.ravel(y).shape[0], 1))
+z_ravel = np.ravel(z).reshape((np.ravel(z).shape[0], 1))
+
+# create the design matrix
+X = create_X_2D(degree, x_ravel, y_ravel)
+
+# train test split the data
+X_train, X_test, z_train, z_test = train_test_split(X, z_ravel, test_size=0.25, random_state=seed)
+
+# fit OLS model
+betas = ols(X_train, z_train)
+# predictions
+z_pred = X_train @ betas
+z_tilde = X_test @ betas
+
+# scaling of the data (optional)
+mean_X = np.mean(X_train, axis=0)
+X_train_scaled = X_train - mean_X
+X_test_scaled = X_test - mean_X
+
+mean_z = np.mean(z_train)
+z_train_scaled = z_train - mean_z
+z_test_scaled = z_test - mean_z
+
+# fit OLS model to franke function
+betas_scaled = ols(X_train_scaled, z_train_scaled)
+# predictions
+z_pred_scaled = X_train_scaled @ betas_scaled
+z_tilde_scaled = X_test_scaled @ betas_scaled
+
+# compute the confidence interval of the betas
+# we know that sgima^2 = diag(inv((X^T @ X)))
+conf_interval_betas = np.sqrt(np.diag(linalg.pinv(X_train.T @ X_train)))
+conf_interval_betas_scaled = np.sqrt(np.diag(linalg.pinv(X_train_scaled.T @ X_train_scaled)))
+
+
+# prints
+print(f"betas:        {betas.T}")
+print(f"confidence interval: {conf_interval_betas.T}")
+print()
+print(f"betas_scaled: {betas_scaled.T}")
+print(f"confidence interval: {conf_interval_betas_scaled.T}")
+print()
+print("Unscaled: ")
+print(f"MSE train: {mean_squared_error(z_train, z_pred)} ")
+print(f"R2 train: {r2_score(z_train, z_pred)} ")
+print(f"MSE test: {mean_squared_error(z_test, z_tilde)} ")
+print(f"R2 test: {r2_score(z_test, z_tilde)} ")
+print("Scaled: ")
+print(f"MSE train_scaled: {mean_squared_error(z_train_scaled, z_pred_scaled)} ")
+print(f"R2 train_scaled: {r2_score(z_train_scaled, z_pred_scaled)} ")
+print(f"MSE test_scaled: {mean_squared_error(z_test_scaled, z_tilde_scaled)} ")
+print(f"R2 test_scaled: {r2_score(z_test_scaled, z_tilde_scaled)} ")
+
+
+# confidence interval plots
+plt.figure("confidence intervals for beta", figsize=(7, 9), dpi=80)
+
+ax = plt.subplot(211)
+plt.errorbar(np.arange(betas.shape[0]), betas, yerr=2*conf_interval_betas, fmt='-ob', capsize=4)
+plt.title("unscaled")
+plt.xlim((-1, betas.shape[0]+1))
+plt.xlabel(r"$i$")
+plt.ylabel(r"$\beta_i \pm 2\sigma$")
+
+ax = plt.subplot(212)
+plt.errorbar(np.arange(betas_scaled.shape[0]), betas_scaled, yerr=2*conf_interval_betas_scaled, fmt='-ob', capsize=4)
+plt.xlim((-1, betas.shape[0]+1))
+plt.title("scaled")
+plt.xlabel(r"$i$")
+plt.ylabel(r"$\beta_i \pm 2\sigma$")
+
+plt.show()
+
+# plot the franke function
+fig = plt.figure("Franke Function", figsize=(8, 6), dpi=80)
+ax = plt.axes(projection='3d')
+
+# Plot the surface.
+surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+# Customize the z axis.
+ax.set_zlim(-0.10, 1.40)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+# plt.show()
+
+
---
+++ project_1/functions.py
@@ -0,0 +1,102 @@
+import numpy as np
+
+def create_X_2D(degree: int, X: np.matrix, Y: np.matrix):
+    """
+        Create the design matrix in the form of a Vandermonde matrix for one or two
+        dimensional data set. The matrix is of the form
+
+        [[1 x_1 y_1 x_1^2 x_1y_1 y_1^2 ... y_1^(degree)]
+            ...
+            [1 x_n y_n x_n^2 x_ny_n y_n^2 ... y_n^(degree)]]
+
+        in 2D.
+        
+        Parameters: 
+            degree (int): degree of the polynomial to fit (p)
+            X (numpy array): The input data points on the X axis (n)
+            Y (numpy array): The input data points on the Y axis (n)
+        
+        Returns: 
+            (numpy array): (n x p) dimensional matrix, 
+            where n is number of datapoints and p is the degree 
+            plus p = degree*(degree + 1)/2 (2-variable input)
+    """
+    
+    design_matrix = np.ones((len(X), int((degree + 1) * (degree + 2) / 2))) # First column of design matrix is 1
+    
+    # The number of features are 1 + 2 + ... + (degree+1) = (degree+1)*(degree+2)/2
+    for i in range(1, degree + 1): # First column is 1, so we skip it
+        q = int(i * (i + 1) / 2) # 1 + 2 + ... + i
+        
+        for k in range(i + 1):
+            design_matrix[:, q + k] = (X[:, 0] ** (i - k)) * (Y[:, 0] ** k)
+    
+    return design_matrix
+
+def franke_function(X, Y):
+    """
+        Franke Function
+        
+        Parameters: 
+            X (numpy array): mesh x data
+            Y (numpy array): mesh y data
+            
+        Returns: 
+            (numpy array): franke functon over X and Y
+    """
+    
+    term1 = 0.75*np.exp(-(0.25*(9*X-2)**2) - 0.25*((9*Y-2)**2))
+    term2 = 0.75*np.exp(-((9*X+1)**2)/49.0 - 0.1*(9*Y+1))
+    term3 = 0.5*np.exp(-(9*X-7)**2/4.0 - 0.25*((9*Y-3)**2))
+    term4 = -0.2*np.exp(-(9*X-4)**2 - (9*Y-7)**2)
+    return term1 + term2 + term3 + term4
+    
+def mean_squared_error(y_data: np.matrix, y_model: np.matrix):
+    """
+        Compute Mean Squared Error
+        
+        Parameters:
+            y_data (numpy array): input data points to compare against
+            y_model (numpy array): predicted data
+        
+        Returns:
+            (float) the computed Mean Squared Error
+    """
+    
+    return np.sum(np.power(y_data-y_model, 2)) / np.size(y_model)
+
+def r2_score(y_data: np.matrix, y_model: np.matrix):
+    """
+        Compute R2 score
+        
+        Parameters:
+            y_data (numpy array) input data points to compare against
+            y_model (numpy array) predicted data
+            
+        Returns:
+            (float) the computed R2 score
+    """
+    
+    return 1 - np.sum((y_data - y_model)**2) / np.sum((y_data - np.mean(y_data))**2)
+
+def ols(X_train, y_train):
+    """
+        Given a design matrix and a (training) data set, returns an evaluator function object that can be given additional data to make predictions
+        Predictions will be based off OLS for this model
+        
+        Parameters:
+            X_train (numpy matrix) design matrix for training
+            y_train (numpy array) target data for traning
+        
+        Returns:
+            (numpy array) optimal coefficients (beta) of the linear regression
+    """ 
+    
+    return np.linalg.pinv(X_train.T @ X_train) @ X_train.T @ y_train
+
+
+
+
+
+
+
