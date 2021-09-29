5530fb6c33e7c6f260ba824db90a28187bebab31
João Inácio <jgci2000@hotmail.com>
2021-09-29 09:13:29 +0200

    just to pull

--- project_1/exercise_2.py
+++ project_1/exercise_2.py
@@ -12,10 +12,10 @@ from functions import create_X_2D, ols, franke_function, mean_squared_error, r2_
 
 # parameters
 max_degree = 15
-max_bootstrap_cycle = 15
+max_bootstrap_cycle = 50
 a = 0
 b = 1
-n = 50
+n = 1000
 noise = 0.2
 
 # random number generator
@@ -26,8 +26,8 @@ rng = np.random.default_rng(np.random.MT19937(seed=seed))
 # the generation can be randomly distributed
 # x = np.linspace(a, b, n)
 # y = np.linspace(a, b, n)
-x = np.sort(rng.uniform(a, b, n))
-y = np.sort(rng.uniform(a, b, n))
+x = np.sort(rng.uniform(a, b, int(np.sqrt(n))))
+y = np.sort(rng.uniform(a, b, int(np.sqrt(n))))
 
 # create a meshgrid and compute the franke function
 x, y = np.meshgrid(x, y)
@@ -41,16 +41,17 @@ x_ravel = np.ravel(x).reshape((np.ravel(x).shape[0], 1))
 y_ravel = np.ravel(y).reshape((np.ravel(y).shape[0], 1))
 z_ravel = np.ravel(z).reshape((np.ravel(z).shape[0], 1))
 
-# mse[0, :] -> train 
+# mse[0, :] -> train
 # mse[1, :] -> test
-mse = np.zeros((2, max_degree))
-var = np.zeros((2, max_degree))
-bias = np.zeros((2, max_degree))
+mse_train = np.zeros(max_degree)
+mse_test = np.zeros(max_degree)
+var = np.zeros(max_degree)
+bias = np.zeros(max_degree)
 
 for i, deg in enumerate(range(1, max_degree + 1)):
     # create X, split and scale
     X = create_X_2D(deg, x_ravel, y_ravel)
-
+    
     # loop over bootstrap cycles
     for bootstrap_cycle in range(max_bootstrap_cycle):
         print(f"bootstrap cycle {bootstrap_cycle+1}/{max_bootstrap_cycle} with degree {deg}/{max_degree} ", end="\r")
@@ -67,32 +68,32 @@ for i, deg in enumerate(range(1, max_degree + 1)):
         z_tilde = X_test @ betas
         
         # compute MSE and R2 score
-        mse[0, i] += mean_squared_error(z_train, z_pred)
-        mse[1, i] += mean_squared_error(z_test, z_tilde)
+        mse_train[i] += mean_squared_error(z_train, z_pred)
+        mse_test[i] += mean_squared_error(z_test, z_tilde)
         
-        # var[0, i] += variance(z_pred)
-        var[1, i] += variance(z_tilde)
+        var[i] += variance(z_tilde)
         
-        # bias[0, i] += bias_squared(z_train, z_pred)
-        bias[1, i] += bias_squared(z_test, z_tilde)
-
-mse /= max_bootstrap_cycle 
+        bias[i] += bias_squared(z_test, z_tilde)
+        
+mse_train /= max_bootstrap_cycle
+mse_test /= max_bootstrap_cycle 
 var /= max_bootstrap_cycle
 bias /= max_bootstrap_cycle
 
 plt.figure("MSE vs complexity")
-plt.plot(np.arange(1, max_degree + 1), mse[0, :], '-b', label='MSE train')
-plt.plot(np.arange(1, max_degree + 1), mse[1, :], '-r', label='MSE test')
+plt.plot(np.arange(1, max_degree + 1), mse_train, '-b', label='MSE train')
+plt.plot(np.arange(1, max_degree + 1), mse_test, '-r', label='MSE test')
+plt.ylim((0, np.max(mse_test) + 0.20))
 plt.xlabel(r"$degree$")
 plt.ylabel(r"$MSE$")
 plt.legend()
 
 plt.figure("MSE vs Bias^2 + Var")
-plt.plot(np.arange(1, max_degree + 1), mse[1, :], '-r', label='MSE')
-plt.plot(np.arange(1, max_degree + 1), var[1, :], '--g', label='var')
-plt.plot(np.arange(1, max_degree + 1), bias[1, :], '--g', label='bias', alpha=0.5)
-plt.plot(np.arange(1, max_degree + 1), bias[1, :] + var[1, :], '--b', label='var+bias')
-plt.ylim((0, np.max(bias[1, :] + var[1, :]) + 0.10))
+plt.plot(np.arange(1, max_degree + 1), mse_test, '-r', label='MSE')
+plt.plot(np.arange(1, max_degree + 1), var, '--g', label='var')
+plt.plot(np.arange(1, max_degree + 1), bias, '--g', label='bias', alpha=0.35)
+plt.plot(np.arange(1, max_degree + 1), bias + var, '--b', label='var+bias')
+plt.ylim((0, np.max(bias + var) + 0.10))
 plt.xlabel(r"$degree$")
 plt.ylabel(r"$MSE$")
 plt.legend()
