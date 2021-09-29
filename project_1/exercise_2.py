132a89999b589d62c9b4b92f10c538bbb8c3689a
João Inácio <jgci2000@hotmail.com>
2021-09-29 10:51:23 +0200

    exercise 2 correct

--- project_1/exercise_2.py
+++ project_1/exercise_2.py
@@ -8,38 +8,35 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from sklearn.model_selection import train_test_split
 from sklearn.utils import resample
 
-from functions import create_X_2D, ols, franke_function, mean_squared_error, r2_score, scale_mean, bias_squared, variance
+from time import time
+
+from functions import create_X_2D, ols, franke_function, mean_squared_error, r2_score, scale_mean#, bias_squared, variance
+
 
 # parameters
 max_degree = 15
 max_bootstrap_cycle = 50
 a = 0
 b = 1
-n = 1000
+n = 800
 noise = 0.2
 
 # random number generator
-seed = 0
+seed = int(time())
 rng = np.random.default_rng(np.random.MT19937(seed=seed))
 
 # generate x and y data
 # the generation can be randomly distributed
-# x = np.linspace(a, b, n)
-# y = np.linspace(a, b, n)
-x = np.sort(rng.uniform(a, b, int(np.sqrt(n))))
-y = np.sort(rng.uniform(a, b, int(np.sqrt(n))))
+# x = np.linspace(a, b, (n, 1))
+# y = np.linspace(a, b, (n, 1))
+x = rng.uniform(a, b, (n, 1))
+y = rng.uniform(a, b, (n, 1))
 
-# create a meshgrid and compute the franke function
-x, y = np.meshgrid(x, y)
+# compute the franke function
 z = franke_function(x, y)
 
 # add noise to the data
 z += noise * rng.normal(0, 1, z.shape)
-
-# ravel the data 
-x_ravel = np.ravel(x).reshape((np.ravel(x).shape[0], 1))
-y_ravel = np.ravel(y).reshape((np.ravel(y).shape[0], 1))
-z_ravel = np.ravel(z).reshape((np.ravel(z).shape[0], 1))
 
 # mse[0, :] -> train
 # mse[1, :] -> test
@@ -48,43 +45,49 @@ mse_test = np.zeros(max_degree)
 var = np.zeros(max_degree)
 bias = np.zeros(max_degree)
 
+# create the design matrix, split it and scale it
+X_max_deg = create_X_2D(max_degree, x, y)
+X_train_, X_test_, z_train, z_test = train_test_split(X_max_deg, z, test_size=0.25)
+X_train_, X_test_, z_train, z_test = scale_mean(X_train_, X_test_, z_train, z_test)
+
+max_features = lambda deg: int((deg) * (deg + 1) / 2)
+
 for i, deg in enumerate(range(1, max_degree + 1)):
-    # create X, split and scale
-    X = create_X_2D(deg, x_ravel, y_ravel)
+    # select the important features
+    X_train = X_train_[:, :max_features(deg + 1)]
+    X_test = X_test_[:, :max_features(deg + 1)]
+    
+    z_tilde_all = np.zeros((z_test.shape[0], max_bootstrap_cycle))
     
     # loop over bootstrap cycles
     for bootstrap_cycle in range(max_bootstrap_cycle):
         print(f"bootstrap cycle {bootstrap_cycle+1}/{max_bootstrap_cycle} with degree {deg}/{max_degree} ", end="\r")
         
         # split and scale the data
-        X_train, X_test, z_train, z_test = train_test_split(X, z_ravel, test_size=0.25)
-        X_train, z_train = resample(X_train, z_train)
-        X_train, X_test, z_train, z_test = scale_mean(X_train, X_test, z_train, z_test)
+        X_train_resampled, z_train_resampled = resample(X_train, z_train)
         
         # fit OLS model to franke function
-        betas = ols(X_train, z_train)
+        betas = ols(X_train_resampled, z_train_resampled)
         # predictions
-        z_pred = X_train @ betas
+        z_pred = X_train_resampled @ betas
         z_tilde = X_test @ betas
         
-        # compute MSE and R2 score
-        mse_train[i] += mean_squared_error(z_train, z_pred)
-        mse_test[i] += mean_squared_error(z_test, z_tilde)
+        z_tilde_all[:, bootstrap_cycle] = z_tilde.reshape((z_tilde.shape[0], ))
         
-        var[i] += variance(z_tilde)
-        
-        bias[i] += bias_squared(z_test, z_tilde)
-        
-mse_train /= max_bootstrap_cycle
-mse_test /= max_bootstrap_cycle 
-var /= max_bootstrap_cycle
-bias /= max_bootstrap_cycle
+        mse_train[i] += mean_squared_error(z_train_resampled, z_pred)
+    
+    # compute MSE, BIAS and VAR
+    mse_train[i] /= max_bootstrap_cycle
+    mse_test[i] = mean_squared_error(z_test, z_tilde_all)
+    bias[i] = np.mean((z_test.reshape(z_test.shape[0], ) - np.mean(z_tilde_all, axis=1))**2)
+    var[i] = np.mean(np.var(z_tilde_all, axis=1))
+
+# plots
 
 plt.figure("MSE vs complexity")
 plt.plot(np.arange(1, max_degree + 1), mse_train, '-b', label='MSE train')
 plt.plot(np.arange(1, max_degree + 1), mse_test, '-r', label='MSE test')
-plt.ylim((0, np.max(mse_test) + 0.20))
-plt.xlabel(r"$degree$")
+plt.xlabel(r"complexity")
 plt.ylabel(r"$MSE$")
 plt.legend()
 
@@ -93,11 +96,9 @@ plt.plot(np.arange(1, max_degree + 1), mse_test, '-r', label='MSE')
 plt.plot(np.arange(1, max_degree + 1), var, '--g', label='var')
 plt.plot(np.arange(1, max_degree + 1), bias, '--g', label='bias', alpha=0.35)
 plt.plot(np.arange(1, max_degree + 1), bias + var, '--b', label='var+bias')
-plt.ylim((0, np.max(bias + var) + 0.10))
-plt.xlabel(r"$degree$")
+plt.xlabel(r"complexity")
 plt.ylabel(r"$MSE$")
 plt.legend()
-
 
 plt.show()
     
