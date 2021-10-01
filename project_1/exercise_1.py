fixed error with var(betas) calculation

project_1/exercise_1.py
@@ -26,7 +26,7 @@ r2_test = np.zeros((2, max_degree))
 
 features_5 = int((6) * (7) / 2)
 betas_5 = np.zeros((2, features_5))
-var_betas_5 = np.zeros((2, features_5))
+std_betas_5 = np.zeros((2, features_5))
 
 for i, deg in enumerate(range(1, max_degree + 1)):
     print(f"degree: {deg}/{max_degree}", end="\r")
@@ -43,22 +43,22 @@ for i, deg in enumerate(range(1, max_degree + 1)):
     if deg == 5:
         betas_5[0, :] = betas_scaled.reshape((betas_scaled.shape[0], ))
         betas_5[1, :] = betas_unscaled.reshape((betas_unscaled.shape[0], ))
-        var_betas_5[0, :] = np.sqrt(var_betas_scaled.reshape((betas_scaled.shape[0], )))
-        var_betas_5[1, :] = np.sqrt(var_betas_unscaled.reshape((betas_unscaled.shape[0], )))
+        std_betas_5[0, :] = np.sqrt(var_betas_scaled.reshape((betas_scaled.shape[0], )))
+        std_betas_5[1, :] = np.sqrt(var_betas_unscaled.reshape((betas_unscaled.shape[0], )))
 
 
 # confidence interval beta values plots
 plt.figure("Confidence intervals for beta values", figsize=(7, 9), dpi=80)
 
 ax = plt.subplot(211)
-plt.errorbar(np.arange(betas_5[0, :].shape[0]), betas_5[0], yerr=2*var_betas_5[0, :], fmt='xb', capsize=4)
+plt.errorbar(np.arange(betas_5[0, :].shape[0]), betas_5[0], yerr=2*std_betas_5[0, :], fmt='xb', capsize=4)
 plt.title("scaled")
 plt.xlim((-1, betas_5[0].shape[0]+1))
 plt.xlabel(r"$i$")
 plt.ylabel(r"$\beta_i \pm 2\sigma$")
 
 ax = plt.subplot(212)
-plt.errorbar(np.arange(betas_5[1, :].shape[0]), betas_5[1, :], yerr=2*var_betas_5[1, :], fmt='xb', capsize=4)
+plt.errorbar(np.arange(betas_5[1, :].shape[0]), betas_5[1, :], yerr=2*std_betas_5[1, :], fmt='xb', capsize=4)
 plt.xlim((-1, betas_5[1, :].shape[0]+1))
 plt.title("unscaled scaled")
 plt.xlabel(r"$i$")

project_1/functions.py
@@ -160,6 +160,7 @@ class Regression():
         
         self.max_degree = max_degree
         self.rng = rng
+        self.noise = noise
         
         self.x = rng.uniform(0, 1, (n, 1))
         self.y = rng.uniform(0, 1, (n, 1))
@@ -191,7 +192,7 @@ class Regression():
             z_test = self.z_test
         
         betas = ols(X_train, z_train)
-        var_betas = np.diag(np.linalg.pinv(X_test.T @ X_test))
+        var_betas = self.noise**2 * np.diag(np.linalg.pinv(X_test.T @ X_test))
         
         z_pred = X_train @ betas
         z_tilde = X_test @ betas
@@ -223,7 +224,7 @@ class Regression():
             betas = ols(X_train_resampled, z_train_resampled)
             # predictions
             z_tilde_all[:, bootstrap_cycle] = (X_test @ betas).reshape((self.z_test.shape[0], ))
-            
+
         # compute MSE, BIAS and VAR
         mse_test = mean_squared_error(self.z_test, z_tilde_all)
         bias = np.mean((self.z_test.reshape(self.z_test.shape[0], ) - np.mean(z_tilde_all, axis=1))**2)