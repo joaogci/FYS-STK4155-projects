212076b783039f6c5473b73f3e056eba2e5837f0
Joao Inacio <jgci2000@hotmail.com>
2021-10-06 09:31:42 +0200

    added intercept for lasso

--- project_1/functions.py
+++ project_1/functions.py
@@ -239,6 +239,8 @@ class Regression():
                 # predictions
                 z_tilde_all[:, bootstrap_cycle] = (X_test @ betas).reshape((self.z_test.shape[0], ))
             else:           # lasso with sklearn
+                X_train[:, 0] = 1
+                X_test[:, 0] = 1
                 lasso = Lasso(alpha=alpha, fit_intercept=False, normalize=True,tol=1e-2, max_iter=1e8)
                 lasso.fit(X_train_resampled, z_train_resampled)
                 z_tilde_all[:, bootstrap_cycle] = lasso.predict(X_test).reshape((self.z_test.shape[0], ))
@@ -284,6 +286,8 @@ class Regression():
                 betas = ols(X_train, z_train, lmd=lmd)
                 z_tilde = X_test @ betas
             else:           # lasso
+                X_train[:, 0] = 1
+                X_test[:, 0] = 1
                 lasso = Lasso(alpha=alpha, fit_intercept=False, normalize=True, tol=1e-2, max_iter=1e8)
                 lasso.fit(X_train, z_train)
                 z_tilde = lasso.predict(X_test)
