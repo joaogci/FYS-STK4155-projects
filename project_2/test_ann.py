bde169a0832fccb3da6e0380c51e30ab46920f85
Jon <jonathan-kings@hotmail.com>
2021-10-19 11:50:20 +0200

    Update test file with XOR/AND/OR test

--- project_2/test_ann.py
+++ project_2/test_ann.py
@@ -8,14 +8,30 @@ from NeuralNetwork.Model import Model
 
 model = Model(2, random_state=0)
 model.add_layer(HiddenLayer(100, Sigmoid()))
-model.add_layer(OutputLayer(2, Sigmoid())) # <- should probably be using softmax instead for the output layer in classification problems!
+model.add_layer(OutputLayer(3, Sigmoid())) # <- should probably be using softmax instead for the output layer in classification problems!
 
-outputs = model.feed_forward(np.matrix([
+# Train for XOR, AND, OR as a test
+inputs = np.matrix([
     [0, 0],
     [0, 1],
     [1, 0],
     [1, 1]
-]))
+])
+targets = np.matrix([
+    [0, 0, 0],
+    [1, 0, 1],
+    [1, 0, 1],
+    [0, 1, 1]
+])
+
+print('\nBefore training:')
+outputs = model.feed_forward(inputs)
 # To run with a single input set instead of several, the following is possible:
 # outputs = model.feed_forward([0, 0])
 print(outputs)
+
+
+model.back_prop(outputs, targets, learning_rate=0.1)
+
+print('\nAfter training:')
+print(model.feed_forward(inputs))
