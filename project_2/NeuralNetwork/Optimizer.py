a83fca6d78f49f57ff5394a08fa8cab5a30ebb61
João Inácio <jgci2000@hotmail.com>
2021-10-20 11:36:44 +0200

    new comments on gradient descent and started SGD

--- project_2/NeuralNetwork/Optimizer.py
+++ project_2/NeuralNetwork/Optimizer.py
@@ -8,17 +8,18 @@ class Optimizer(ABC):
     """
     
     @abstractmethod
-    def optimize(self) -> ...:
+    def optimize(self, eta: float, tol: float = 1e-7, iter_max: int = 1e5) -> ...:
         """
-            
+            Finds the minimum of the inpute CostFunction using the analytical expression for the gradient.
+            If there is no analytical expression for the gradient, it uses autograd. 
         """
-        print('Error: cannot instantiate/use the default Optimzer class - use a base class that overrides tbd()!')
+        print('Error: cannot instantiate/use the default Optimzer class - use a base class that overrides optimize()!')
         return None
     
     @abstractmethod
-    def optimize_autograd(self) -> ...:
+    def optimize_autograd(self, eta: float, tol: float = 1e-7, iter_max: int = 1e5) -> ...:
         """
-            
+            Finds the minimum of the inpute CostFunction using autograd.
         """
-        print('Error: cannot instantiate/use the default Optimzer class - use a base class that overrides tbd()!')
+        print('Error: cannot instantiate/use the default Optimzer class - use a base class that overrides optimize_autograd()!')
         return None
