a2b0c8384193635ad831ad555cdba0cc427ec233
Jon <jonathan-kings@hotmail.com>
2021-10-20 12:25:02 +0200

    Add softmax

---
+++ project_2/NeuralNetwork/activation/Softmax.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+from .ActivationFunction import ActivationFunction
+
+class Softmax(ActivationFunction):
+    """
+        Softmax activation function
+        Note: since Softmax needs ALL of the layer's weighted sums at once instead of just a single float, the inputs to call and d MUST be matrices
+    """
+
+    def __call__(self, x: np.matrix) -> np.matrix:
+        """
+            Returns f(x)
+        """
+        # ln(sum(a_i)) = ln(a_0) + ln(sum(a_i / a_0))
+        expTerm = np.exp(x)
+        return expTerm / np.sum(expTerm, axis=1, keepdims=True)
+
+    def d(self, x: np.matrix) -> np.matrix:
+        """
+            Returns f'(x)
+        """
+        print('\n-Unimplemented softmax derivative-\n')
+        return None
