a80522b7b490f6bd3cd94ab168174f46011504ed
João Inácio <jgci2000@hotmail.com>
2021-11-01 09:46:23 +0100

    finished SGD implementation

--- project_2/NeuralNetwork/StochasticGradientDescent.py
+++ project_2/NeuralNetwork/StochasticGradientDescent.py
@@ -5,7 +5,7 @@ from .cost_function.CostFunction import CostFunction
 
 class StochasticGradientDescent(Optimizer):
     
-    def __init__(self, cost_function: CostFunction):
+    def __init__(self, cost_function: CostFunction, size_minibatches: int, t0: int, t1: int, rng: np.random.Generator):
         """
             Finds the minimum of the inpute CostFunction using the analytical expression for the gradient.
             If there is no analytical expression for the gradient, it uses autograd. 
@@ -14,7 +14,14 @@ class StochasticGradientDescent(Optimizer):
         """
         self.cost_function = cost_function
         self.n_features = cost_function.n_features
-        
+        self.n_batches = cost_function.n // size_minibatches
+        self.size_minibatches = size_minibatches
+        self.t0 = t0
+        self.t1 = t1
+        self.eta = lambda t: self.t0 / (t + self.t1)
+        self.rng = rng
+         
+    
     def optimize(self, eta: float, tol: float = 1e-7, iter_max: int = 1e5) -> np.matrix:
         """
             Finds the minimum of the inpute CostFunction using the analytical expression for the gradient.
@@ -24,16 +31,19 @@ class StochasticGradientDescent(Optimizer):
                 tol (float): tolerance
                 iter_max (int): maximum number of iterations
         """
-        beta = np.zeros((self.n_features, 1))
+        theta = np.zeros((self.n_features, 1))
         
-        for i in range(iter_max):
-            dif = - eta * self.cost_function.grad_C(beta)
-            if np.abs(dif) <= tol:
-                break
+        for epoch in range(1, iter_max + 1):
+            for i in range(self.n_batches):
+                k = self.rng.integers(self.n_batches)
+                dif = - self.eta(epoch * self.n_batches + i) * self.cost_function.grad_C(theta, indx=np.arange(k*self.size_minibatches, (k+1)*self.size_minibatches, 1))
+                
+                if np.abs(dif) <= tol:
+                    break
+                
+                theta = theta + dif
             
-            beta = beta + dif
-            
-        return beta
+        return theta
     
     def optimize_autograd(self, eta: float, tol: float = 1e-7, iter_max: int = 1e5) -> np.matrix:
         """
@@ -44,13 +54,17 @@ class StochasticGradientDescent(Optimizer):
                 tol (float): tolerance
                 iter_max (int): maximum number of iterations
         """
-        beta = np.zeros((self.n_features, 1))
+        theta = np.zeros((self.n_features, 1))
         
-        for i in range(iter_max):
-            dif = - eta * self.cost_function.grad_C_autograd(beta)
-            if np.abs(dif) <= tol:
-                break
+        for epoch in range(1, iter_max + 1):
+            for i in range(self.n_batches):
+                k = self.rng.integers(self.n_batches)
+                dif = - self.eta(epoch * self.n_batches + i) * self.cost_function.grad_C_autograd(theta, indx=np.arange(k*self.size_minibatches, (k+1)*self.size_minibatches, 1))
+                
+                if np.abs(dif) <= tol:
+                    break
+                
+                theta = theta + dif
             
-            beta = beta + dif
-            
-        return beta
+        return theta
+    
