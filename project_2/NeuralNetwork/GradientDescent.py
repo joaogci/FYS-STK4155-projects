fixed gradient descent methods

project_2/NeuralNetwork/GradientDescent.py
@@ -8,7 +8,6 @@ class GradientDescent(Optimizer):
     def __init__(self, cost_function: CostFunction, eta: float):
         """
             Finds the minimum of the inpute CostFunction using the analytical expression for the gradient.
-            If there is no analytical expression for the gradient, it uses autograd. 
             Parameters:
                 cost_function (CostFunction): cost function to minimize
         """
@@ -19,7 +18,6 @@ class GradientDescent(Optimizer):
     def optimize(self, tol: float = 1e-7, iter_max: int = int(1e5)) -> np.matrix:
         """
             Finds the minimum of the inpute CostFunction using the analytical expression for the gradient.
-            If there is no analytical expression for the gradient, it uses autograd. 
             Parameters:
                 eta (float): learning rate
                 tol (float): tolerance
@@ -30,28 +28,11 @@ class GradientDescent(Optimizer):
         for epoch in range(1, iter_max + 1):
             dif = - self.eta * self.cost_function.grad_C(theta)
             if np.linalg.norm(dif) <= tol:
+                print()
                 break
             
             theta = theta + dif
+            print(epoch, end='\r')
             
         return theta
     
-    def optimize_autograd(self, tol: float = 1e-7, iter_max: int = int(1e5)) -> np.matrix:
-        """
-            Finds the minimum of the inpute CostFunction using the analytical expression for the gradient.
-            If there is no analytical expression for the gradient, it uses autograd. 
-            Parameters:
-                eta (float): learning rate
-                tol (float): tolerance
-                iter_max (int): maximum number of iterations
-        """
-        theta = np.zeros(self.n_features)        
-        
-        for epoch in range(1, iter_max + 1):
-            dif = - self.eta * self.cost_function.grad_C_autograd(theta)
-            if np.linalg.norm(dif) <= tol:
-                break
-            
-            theta = theta + dif
-            
-        return theta

project_2/NeuralNetwork/StochasticGradientDescent.py
@@ -25,54 +25,24 @@ class StochasticGradientDescent(Optimizer):
     def optimize(self, tol: float = 1e-7, iter_max: int = int(1e5)) -> np.matrix:
         """
             Finds the minimum of the inpute CostFunction using the analytical expression for the gradient.
-            If there is no analytical expression for the gradient, it uses autograd. 
             Parameters:
                 eta (float): learning rate
                 tol (float): tolerance
                 iter_max (int): maximum number of iterations
         """
         theta = np.zeros(self.n_features)
-        broken = False
         
         for epoch in range(1, iter_max + 1):
             for i in range(self.n_batches):
                 k = self.rng.integers(self.n_batches)
                 dif = - self.eta(epoch * self.n_batches + i) * self.cost_function.grad_C(theta, indx=np.arange(k*self.size_minibatches, (k+1)*self.size_minibatches, 1))
-                if np.linalg.norm(dif) <= tol:
-                    broken = True
-                    break
+                if np.linalg.norm(dif, ord=np.inf) <= tol:
+                    print()
+                    return theta
             
                 theta = theta + dif
-            if broken:
-                break
             print(epoch, end='\r')
             
-        return theta
-    
-    def optimize_autograd(self, tol: float = 1e-7, iter_max: int = int(1e5)) -> np.matrix:
-        """
-            Finds the minimum of the inpute CostFunction using the analytical expression for the gradient.
-            If there is no analytical expression for the gradient, it uses autograd. 
-            Parameters:
-                eta (float): learning rate
-                tol (float): tolerance
-                iter_max (int): maximum number of iterations
-        """
-        theta = np.zeros(self.n_features)
-        broken = False
-
-        for epoch in range(1, iter_max + 1):
-            for i in range(self.n_batches):
-                k = self.rng.integers(self.n_batches)
-                dif = - self.eta(epoch * self.n_batches + i) * self.cost_function.grad_C_autograd(theta, indx=np.arange(k*self.size_minibatches, (k+1)*self.size_minibatches, 1))
-                if np.linalg.norm(dif) <= tol:
-                    broken = True
-                    break
-                
-                theta = theta + dif
-            if broken:
-                break
-            print(epoch, end='\r')
-            
+        print("Need to increase iter_max to achieve desired tol.")
         return theta
     