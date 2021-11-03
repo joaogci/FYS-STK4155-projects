Correct backpropagation algorithm

project_2/NeuralNetwork/Layer.py
@@ -39,32 +39,35 @@ class Layer(ABC):
         """
         self._weights = rng.uniform(-1, 1, (self._size, input_size))
 
-    def forward(self, inputs: np.matrix) -> np.matrix:
+    def forward(self, inputs: np.matrix) -> tuple:
         """
             Runs through the layer once with a given list of inputs and returns the outputs obtained
             Parameters:
                 inputs (np.matrix): Inputs to run through the nodes (must match size of weights/biases!)
             Returns:
                 (np.matrix): Outputs from the different nodes - size corresponds to size of the layer
+                (np.matrix): Same outputs without activation function
         """
         
         if self._weights is None or self._biases is None or len(self._weights) != self._biases.shape[0]:
             print('\033[91mLayer hasn\'t been assigned weights! Ensure init_weights is called before starting.\033[0m')
-            return None
+            return None, None
 
         if inputs.shape[1] != len(self._weights[0]):
             print('\033[91mLayer hasn\'t been initialised with the right amount of weights/biases! Expected', len(self._weights[0]), 'inputs, given', inputs.shape, '!\033[0m')
-            return None
+            return None, None
         
         # Accumulate inputs for each node
         # Because of the way we structure the input, we need to transpose inputs and outputs :)
-        return self._activationFn(self._weights @ inputs.T + self._biases).T
+        z = (self._weights @ inputs.T + self._biases).T
+        return self._activationFn(z), z
 
-    def backward(self, inputs: np.matrix, error: np.matrix, learning_rate: float, regularization: float) -> np.matrix:
+    def backward(self, activated_inputs: np.matrix, inputs: np.matrix, error: np.matrix, learning_rate: float, regularization: float) -> np.matrix:
         """
             Gradient descent to optimize the layer
             Parameters:
-                inputs (np.matrix): The inputs the layer receives
+                activated_inputs (np.matrix): The inputs the layer receives
+                inputs (np.matrix): The inputs the layer receives (no activation fn)
                 error (np.matrix): Computed error estimate for the layer
                 learning_rate (float): Learning rate η to use to update the weights & biases
                 regularization (float): Regularization parameter λ to control the rate of descent
@@ -73,8 +76,8 @@ class Layer(ABC):
         """
         # Compute gradients
         # Simple gradient descent
-        weights_gradient = inputs.T @ error
-        bias_gradient = np.sum(error, axis=0)
+        weights_gradient = activated_inputs.T @ error
+        bias_gradient = error
 
         # Add regularization term to weights gradient (might be 0)
         weights_gradient += regularization * self._weights.T
@@ -84,11 +87,12 @@ class Layer(ABC):
         self._biases -= learning_rate * bias_gradient.T
 
         # Return the estimated error in inputs
-        return np.multiply((error @ self._weights), np.multiply(inputs, (1.0 - inputs)))
+        return np.multiply((error @ self._weights), self._activationFn.d(inputs))
 
 
 
 # Declare Hidden and Output layer classes for nicer syntax
+# The only difference between the two is the name
 class HiddenLayer(Layer):
     ...
 

project_2/NeuralNetwork/Model.py
@@ -70,7 +70,8 @@ class Model:
                 training (bool): If true, will return hidden layer activations alongside the actual outputs
             Returns:
                 (np.matrix|list<float>): Outputs obtained out of the output layer after running through all layers
-                (list<np.matrix>): Hidden layer activation outputs, returned only if `training` was `true`
+                (list<np.matrix>): Hidden layer activated outputs, returned only if `training` was `true`
+                (list<np.matrix>): Hidden layer outputs (no activation function), returned only if `training` was `true`
         """
 
         # If the input is given as a 1D array, we're wanting to use that as a single row in the input matrix (i.e. run with a single set of input data)
@@ -91,10 +92,11 @@ class Model:
         tmp = inputs
         if training:
             a_h = [inputs]
+            z_h = [inputs]
         for layer in self.layers:
 
             # Activate the layer
-            tmp = layer.forward(tmp)
+            tmp, z = layer.forward(tmp)
 
             # If for whatever reason some kind of error occured, the output of forward() will be null
             if tmp is None:
@@ -103,13 +105,14 @@ class Model:
             
             # In training, keep track of hidden layer outputs
             if training and isinstance(layer, HiddenLayer):
+                z_h.append(z)
                 a_h.append(tmp)
         
         # Output of final layer = output of network
         if output_list:
             tmp = tmp[0] # Output as a list if the input was given as such
         if training:
-            return tmp, a_h
+            return tmp, a_h, z_h
         return tmp
 
     def fwd_mse(self, inputs: np.matrix, targets: np.matrix) -> float:
@@ -145,7 +148,7 @@ class Model:
             targs = targets[i]
 
             # Feed forward once to obtain outputs
-            outputs, a_h = self.feed_forward(ins, training=True)
+            outputs, a_h, z_h = self.feed_forward(ins, training=True)
 
             # Dimensionality check
             if outputs.shape != targs.shape or outputs.shape[1] != self.layers[len(self.layers) - 1].get_size():
@@ -156,7 +159,6 @@ class Model:
             # Going backwards from last to first layer
             prev_layer_err = outputs - targs
             for j in range(len(self.layers)-1, -1, -1): # for (let i = len(self.layers) - 1; i >= 0; --i)       (python is fucking garbage)
-
                 # Update layer
-                prev_layer_err = self.layers[j].backward(a_h[j], prev_layer_err, learning_rate, regularization)
+                prev_layer_err = self.layers[j].backward(a_h[j], z_h[j], prev_layer_err, learning_rate, regularization)
         

project_2/NeuralNetwork/activation/Sigmoid.py
@@ -17,4 +17,4 @@ class Sigmoid(ActivationFunction):
         """
             Returns the derivative of the sigmoid f'(x)
         """
-        return self(x) * (1.0 - self(x))
+        return np.multiply(self(x), (1.0 - self(x)))