b2e01714b99add00f795a2b05b3fc71f129a7e37
Jon <jonathan-kings@hotmail.com>
2021-11-03 15:23:13 +0100

    Complete b_NN

--- project_2/part_b_NN.py
+++ project_2/part_b_NN.py
@@ -12,9 +12,10 @@ from functions import *
 from sklearn.model_selection import train_test_split
 
 # params
-n = 1000
+n = 100
 noise = 0
 seed = 1337
+iterations = 1000
 
 # rng 
 rng = np.random.default_rng(np.random.MT19937(seed=seed))
@@ -32,27 +33,27 @@ x = np.ravel(x)
 y = np.ravel(y)
 z = np.ravel(z)
 
-X = create_X_2D(5, x, y)
+X = np.zeros((x.shape[0], 2))
+X[:, 0] = x
+X[:, 1] = y
 
 X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=0.2)
+z_train, z_test = np.matrix(z_train).T, np.matrix(z_test).T
 
 n_data_points = X_train.shape[0]
-n_features = X_train.shape[1]
 
-neural_network = Model(n_features, random_state=seed)
+# input shape = (n, 2)   |   output shape = (n, 1)
+neural_network = Model(2, random_state=seed)
 neural_network.add_layer(HiddenLayer(100, Sigmoid()))
 neural_network.add_layer(HiddenLayer(100, Sigmoid()))
-neural_network.add_layer(OutputLayer(n_features, Linear()))
+neural_network.add_layer(OutputLayer(1, Linear()))
 
 outputs = neural_network.feed_forward(X_train)
 
-print(outputs)
-
 print()
-for i in range(1000):
-    print(int(i / 1000 * 100), '%', end='\r')
-    for j in range(len(z_train)): # For now, back_prop only deals with these one row at a time
-        neural_network.back_prop(X_train[j], z_train[j], learning_rate=1)
+for i in range(iterations):
+    print(int(i / iterations * 100), '%', end='\r')
+    neural_network.back_prop(X_train, z_train, learning_rate=0.02)
 print('100%')
 
 # Print final outputs
@@ -61,9 +62,11 @@ print('\nAfter training:')
 print(neural_network.feed_forward(X_train))
 print('\nTargets:')
 print(z_train)
+print('\nMSE:', neural_network.fwd_mse(X_train, z_train))
 
-print("Test data: ")
+print("\n\nTest data: ")
 print('\nAfter training:')
 print(neural_network.feed_forward(X_test))
 print('\nTargets:')
 print(z_test)
+print('\nMSE:', neural_network.fwd_mse(X_test, z_test))
