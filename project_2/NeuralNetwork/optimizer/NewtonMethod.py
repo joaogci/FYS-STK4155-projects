f80ec1054570bd4affd148501678aab3285df3ec
João Inácio <jgci2000@hotmail.com>
2021-11-07 16:29:25 +0100

    change the way we stop the epochs cycle

--- project_2/NeuralNetwork/optimizer/NewtonMethod.py
+++ project_2/NeuralNetwork/optimizer/NewtonMethod.py
@@ -17,7 +17,7 @@ class NewtonMethod(Optimizer):
         self.cost_function = cost_function
         self.n_features = cost_function.n_features
         
-    def optimize(self, eta: float, random_state: int, tol: float = 1e-7, iter_max: int = int(1e5)) -> np.matrix:
+    def optimize(self, eta: float, random_state: int, tol: float = 1e-7, iter_max: int = int(1e5), verbose: bool = False) -> np.matrix:
         """
             Finds the minimum of the inpute CostFunction using the analytical expression for the gradient.
             Parameters:
@@ -27,27 +27,33 @@ class NewtonMethod(Optimizer):
         """
         self.rng = np.random.default_rng(np.random.MT19937(seed=random_state))
         theta = self.rng.random(self.n_features)
-        prev_error = 1000000
+        error_list = list()
         
         print()
         print("-- Newton's Method --")
         print()        
+        
+        error_list.append(self.cost_function.error(theta))
         
         for epoch in range(1, iter_max + 1):
             grad = self.cost_function.grad_C(theta)
             theta = theta - np.linalg.pinv(self.cost_function.hess_C(theta)) @ grad
             
             error = self.cost_function.error(theta)
-            print(f"[ Epoch: {epoch}/{iter_max}; {self.cost_function.error_name()}: {error} ]")
+            error_list.append(error)
+            print(f"[ Epoch: {epoch}/{iter_max}; {self.cost_function.error_name()}: {error} ]", end='\r')
             
-            if np.abs(prev_error - error) <= tol:
+            if epoch >= 5 and np.abs(np.mean(error_list[-5:]) - error) <= tol:
                 print()
                 print(f"[ Finished training with error: {self.cost_function.error(theta)} ]")
+                if verbose:
+                    return theta, epoch, np.array(error_list)
                 return theta
-            prev_error = error
                 
         print()
         print(f"[ Finished training with error: {self.cost_function.error(theta)} ]")
+        if verbose:
+            return theta, epoch, np.array(error_list)
         return theta
     
     def plot_MSE(self):
--- project_2/NeuralNetwork/optimizer/StochasticGradientDescent.py
+++ project_2/NeuralNetwork/optimizer/StochasticGradientDescent.py
@@ -1,4 +1,5 @@
 
+from math import e
 import numpy as np
 import matplotlib.pyplot as plt
 from .Optimizer import Optimizer
@@ -18,7 +19,7 @@ class StochasticGradientDescent(Optimizer):
         self.n_batches = cost_function.n // size_minibatches
         self.size_minibatches = size_minibatches
     
-    def optimize(self, eta: float, random_state: int, tol: float = 1e-7, iter_max: int = int(1e5)) -> np.matrix:
+    def optimize(self, eta: float, random_state: int, tol: float = 1e-7, iter_max: int = int(1e5), verbose: bool = False) -> np.matrix:
         """
             Finds the minimum of the inpute CostFunction using the analytical expression for the gradient.
             Parameters:
@@ -29,31 +30,37 @@ class StochasticGradientDescent(Optimizer):
         self.rng = np.random.default_rng(np.random.MT19937(seed=random_state))
         theta = self.rng.random(self.n_features)
         self.eta = eta
-        prev_error = 1000000
+        error_list = list()
        
         print()
         print("-- Stochastic Gradient Descent --")
         print()
         
+        error_list.append(self.cost_function.error(theta)) 
+       
         for epoch in range(1, iter_max + 1):
             self.cost_function.perm_data(self.rng)
             
             for i in range(self.n_batches):
-                k = self.rng.integers(self.n_batches)
-                grad = self.cost_function.grad_C(theta, indx=np.arange(k*self.size_minibatches, (k+1)*self.size_minibatches, 1))
+                # k = self.rng.integers(self.n_batches)
+                grad = self.cost_function.grad_C(theta, indx=np.arange(i*self.size_minibatches, (i+1)*self.size_minibatches, 1))
                 theta = theta - self.eta * grad
                 
             error = self.cost_function.error(theta)
-            print(f"[ Epoch: {epoch}/{iter_max}; {self.cost_function.error_name()}: {error} ]")
+            error_list.append(error)
+            print(f"[ Epoch: {epoch}/{iter_max}; {self.cost_function.error_name()}: {error} ]", end='\r')
             
-            if np.abs(prev_error - error) <= tol:
+            if epoch >= 5 and np.abs(np.mean(error_list[-5:]) - error) <= tol:
                 print()
                 print(f"[ Finished training with error: {self.cost_function.error(theta)} ]")
+                if verbose:
+                    return theta, epoch, np.array(error_list)
                 return theta
-            prev_error = error
         
         print()
         print(f"[ Finished training with error: {self.cost_function.error(theta)} ]")
+        if verbose:
+            return theta, epoch, np.array(error_list)
         return theta
         
     def plot_MSE(self):
