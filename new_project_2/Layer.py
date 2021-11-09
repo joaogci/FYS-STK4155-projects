435531a0e578151afff8d3cfb3384340ec5e693b
Johan <jaf1@hotmail.no>
2021-11-09 12:12:48 +0100

    Start adding docstrings.

--- new_project_2/Layer.py
+++ new_project_2/Layer.py
@@ -1,8 +1,23 @@
 import numpy as np
 
 class Layer():
-    
-    def __init__(self, n_nodes: int, n_nodes_prev_layer: int, activation_function: tuple, rng: np.random.Generator):
+    """
+    Layer class for keeping track of weights, biases and activation
+    function for layers in Feed Forward Neural Network.
+    """
+    def __init__(self, n_nodes: int, n_nodes_prev_layer: int, 
+                activation_function: tuple, rng: np.random.Generator):
+        """
+        Initialises the layer with an activation function, weight 
+        matrix, and bias vector.
+        Parameters:
+            n_nodes (int): Number of wanted nodes in the layer
+            n_nodes_prev_layer (int): Number of nodes in previous layer
+            activation_function (tuple): Tuple containing wanted 
+                                         activation function and it's
+                                         derivative.
+            rng (np.random.Generator): Psuedo random number generator
+        """
         self.n_nodes = n_nodes
         self.n_nodes_prev_layer = n_nodes_prev_layer
         
