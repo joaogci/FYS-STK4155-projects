802f31ef389f152abc22fff06f42aa9b6dcabbdb
Jon <jonathan-kings@hotmail.com>
2021-11-10 15:47:26 +0100

    Update franke grid search

--- project_2/grid_search_eta_lambda_franke.py
+++ project_2/grid_search_eta_lambda_franke.py
@@ -14,15 +14,14 @@ from NeuralNetwork.cost_function.LinearRegression import LinearRegression
 from functions import *
 
 
-print('THIS ONE DOESN\'T WORK YET, TEST WITH LOWER VALS BEFORE RUNNING! :)')
-exit()
-
 # params
 n = 1000
 noise = 0.1
 seed = 0
 epochs = 500
 n_nodes = 30
+learning_rates = np.logspace(-7, -3, 5)
+reg_params = np.logspace(-9, -5, 5)
 
 # init data
 rng = np.random.default_rng(np.random.MT19937(seed=seed))
@@ -45,13 +44,13 @@ cost_fn = LinearRegression(X_train, z_train, X_test, z_test)
 nn = Model(2, cost_function=cost_fn, random_state=seed)
 nn.add_layer(HiddenLayer(n_nodes, activation_function=Sigmoid()))
 nn.add_layer(OutputLayer(1, activation_function=Linear()))
-nn.grid_train(X_train, z_train, X_test, z_test, 'sigmoid_grid_search_eta_lambda_franke', plot=True, initial_learning_rate=np.logspace(-5, -1, 5), regularization=np.logspace(-15, -11, 5), epochs=epochs)
+nn.grid_train(X_train, z_train, X_test, z_test, 'sigmoid_grid_search_eta_lambda_franke', plot=True, initial_learning_rate=learning_rates, regularization=reg_params, epochs=epochs)
 
 # Train with grid search - relu
 nn = Model(2, cost_function=cost_fn, random_state=seed)
 nn.add_layer(HiddenLayer(n_nodes, activation_function=ReLU()))
 nn.add_layer(OutputLayer(1, activation_function=Linear()))
-nn.grid_train(X_train, z_train, X_test, z_test, 'relu_grid_search_eta_lambda_franke', plot=True, initial_learning_rate=np.logspace(-5, -1, 5), regularization=np.logspace(-15, -11, 5), epochs=epochs)
+nn.grid_train(X_train, z_train, X_test, z_test, 'relu_grid_search_eta_lambda_franke', plot=True, initial_learning_rate=learning_rates, regularization=reg_params, epochs=epochs)
 
 if __name__ == '__main__':
     plt.show()
