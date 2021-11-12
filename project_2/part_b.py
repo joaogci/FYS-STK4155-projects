c60950fd4c9b90e8fdfa9d1bf44d7f854c95a2c0
João Inácio <jgci2000@hotmail.com>
2021-11-12 10:52:09 +0100

    small changes to part_b

--- project_2/part_b.py
+++ project_2/part_b.py
@@ -45,18 +45,22 @@ X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=0.25, random
 lin_reg = LinearRegression(X_train, z_train, X_test, z_test)
 
 optimizer_SGD = StochasticGradientDescent(lin_reg, size_minibatches=5)
-theta_SGD = optimizer_SGD.optimize(iter_max=int(1e3), eta=0.01, random_state=seed, tol=1e-12)
+theta_SGD = optimizer_SGD.optimize(iter_max=int(1e4), eta=0.01, random_state=seed, tol=0)
 
-optimizer_RMS = RMSprop(lin_reg)
-theta_RMS = optimizer_RMS.optimize(iter_max=int(1e3), eta=0.01, random_state=seed, tol=1e-12)
+optimizer_SGD = StochasticGradientDescent(lin_reg, size_minibatches=5)
+theta_SGD = optimizer_SGD.optimize(iter_max=int(1e4), eta=0.01, random_state=seed, tol=1e-8)
 
 optimizer_newton = NewtonMethod(lin_reg)
-theta_newton = optimizer_newton.optimize(iter_max=int(4e4), eta=0, random_state=seed, tol=1e-12)
+theta_newton = optimizer_newton.optimize(iter_max=int(1e4), eta=0, random_state=seed, tol=0)
 
 theta_ols = ols(X_train, z_train)
 
 print("MSE OLS:", mean_squared_error(z_test, X_test @ theta_ols))
 print("MSE SGD:", mean_squared_error(z_test, X_test @ theta_SGD))
-print("MSE RMS:", mean_squared_error(z_test, X_test @ theta_RMS))
 print("MSE newton:", mean_squared_error(z_test, X_test @ theta_newton))
 
+plot_prediction_3D(theta_ols, 5, show=False, name="ols")
+plot_prediction_3D(theta_SGD, 5, show=False, name="SGD")
+plot_prediction_3D(theta_newton, 5, show=False, name="newton")
+
+plt.show()
