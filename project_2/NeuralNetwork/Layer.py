Fix activation function in backprop

project_2/NeuralNetwork/Layer.py
@@ -75,13 +75,14 @@ class Layer(ABC):
         z = (self._weights @ inputs.T + self._biases).T
         return self._activation_fn(z), z
 
-    def backward(self, activated_inputs: np.matrix, inputs: np.matrix, error: np.matrix, learning_rate: float, regularization: float) -> np.matrix:
+    def backward(self, activated_inputs: np.matrix, inputs: np.matrix, error: np.matrix, prev_activation_fn: ActivationFunction, learning_rate: float, regularization: float) -> np.matrix:
         """
             Gradient descent to optimize the layer
             Parameters:
                 activated_inputs (np.matrix): The inputs the layer receives
                 inputs (np.matrix): The inputs the layer receives (no activation fn)
                 error (np.matrix): Computed error estimate for the layer
+                prev_activation_fn (ActivationFunction): Activation function of the previous layer (l-1)
                 learning_rate (float): Learning rate η to use to update the weights & biases
                 regularization (float): Regularization parameter λ to control the rate of descent
             Returns:
@@ -97,7 +98,7 @@ class Layer(ABC):
         self._biases -= learning_rate * bias_gradient.T
 
         # Return the estimated error in inputs
-        return np.multiply((error @ self._weights), self._activation_fn.d(inputs))
+        return np.multiply((error @ self._weights), prev_activation_fn.d(inputs))
 
 
 

project_2/NeuralNetwork/Model.py
@@ -182,7 +182,8 @@ class Model:
             prev_layer_err = np.multiply(self.cost_function.grad_C_nn(targs, a_h[-1]), self.layers[-1]._activation_fn.d(z_h[-1]))
             for j in range(len(self.layers)-1, -1, -1): # for (let i = len(self.layers) - 1; i >= 0; --i)       (python is fucking garbage)
                 # Update layer
-                prev_layer_err = self.layers[j].backward(a_h[j], z_h[j], prev_layer_err, learning_rate, regularization)
+                prev_activation_fn = self.layers[j-1 if j > 0 else 0]._activation_fn
+                prev_layer_err = self.layers[j].backward(a_h[j], z_h[j], prev_layer_err, prev_activation_fn, learning_rate, regularization)
         return True
     
 