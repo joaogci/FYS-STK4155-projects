da23af818e5b939413751de3562c48f88031da90
João Inácio <jgci2000@hotmail.com>
2021-11-13 17:25:16 +0100

    correlation matrix for the breast cancer datase

---
+++ project_2/corr_cancer.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.datasets import load_breast_cancer
+
+cancer_data = load_breast_cancer()
+
+inputs = cancer_data.data
+target = cancer_data.target
+
+data_frame = pd.DataFrame(inputs, columns=cancer_data.feature_names)
+
+fig, ax = plt.subplots(figsize=(20, 15))
+sns.heatmap(data=data_frame.corr(), annot=True, ax=ax, cbar_kws={'label': 'Correlation'})
+ax.set_title("Covariance Matrix for the Features of the Breast Cancer Dataset")
+
+plt.savefig("./figs/corr_cancer.pdf", dpi=400)
+# plt.show()
+
+
+
+
