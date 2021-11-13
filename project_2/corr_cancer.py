3da79bd94295055a384b2e5f4e552d100e834563
João Inácio <jgci2000@hotmail.com>
2021-11-13 17:35:48 +0100

    changed dimensions

--- project_2/corr_cancer.py
+++ project_2/corr_cancer.py
@@ -15,6 +15,12 @@ data_frame = pd.DataFrame(inputs, columns=cancer_data.feature_names)
 fig, ax = plt.subplots(figsize=(20, 15))
 sns.heatmap(data=data_frame.corr(), annot=True, ax=ax, cbar_kws={'label': 'Correlation'})
 ax.set_title("Covariance Matrix for the Features of the Breast Cancer Dataset")
+plt.subplots_adjust(left=0.11,
+                    bottom=0.1, 
+                    right=1, 
+                    top=0.98, 
+                    wspace=0.3, 
+                    hspace=0.6)
 
 plt.savefig("./figs/corr_cancer.pdf", dpi=400)
 # plt.show()
