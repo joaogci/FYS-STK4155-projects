712b2c49b8d74293cf7516a525185537dc1babc1
João Inácio <jgci2000@hotmail.com>
2021-12-06 18:24:15 +0100

    working code;

--- project_3/NeuralODE.py
+++ project_3/NeuralODE.py
@@ -33,7 +33,7 @@ class NeuralODE():
                 bias_init (float): constant for initialization of bias vector
         """
         if self.n_layers == 0:
-            weights = self.rng.normal(0, 1, size=(n_nodes, self.n_input_nodes)) / (n_nodes * self.n_input_nodes)
+            weights = self.rng.normal(0, 1, size=(n_nodes, self.n_input_nodes))
             biases = self.rng.uniform(0, 1, size=(n_nodes, 1)) * bias_init
             
             self.params.append(np.zeros((n_nodes, 1 + self.n_input_nodes)))
@@ -41,7 +41,7 @@ class NeuralODE():
             self.params[-1][:, 1:] = weights
             self.activation_function.append(activation_function)
         else: 
-            weights = self.rng.normal(0, 1, size=(n_nodes, self.params[-1].shape[0])) / (n_nodes * self.params[-1].shape[0])
+            weights = self.rng.normal(0, 1, size=(n_nodes, self.params[-1].shape[0]))
             biases = self.rng.uniform(0, 1, size=(n_nodes, 1)) * bias_init
             
             self.params.append(np.zeros((n_nodes, 1 + self.params[-1].shape[0])))
@@ -137,31 +137,42 @@ if __name__ == '__main__':
     
     seed = 1337
     
-    N = 50
+    N = 10
     x = np.linspace(0, 1, N)
     
     nn = NeuralODE(1, random_state=seed)
-    nn.add_layer(32, sigmoid()[0])
-    nn.add_layer(32, sigmoid()[0])
+    nn.add_layer(100, sigmoid()[0])
+    # nn.add_layer(50, sigmoid()[0])
+    # nn.add_layer(25, sigmoid()[0])
     nn.add_layer(1, linear()[0])
     
     ff = nn.feed_forward()
+    # alpha = 2
+    # A = 1
+    # f0 = 1.2
+    # euler = lambda x: alpha * x * (A - x)
+    # trial = lambda x, params: f0 + x * ff(x, params)
+    # ode = lambda x, params: alpha * trial(x, params) * (A - trial(x, params))
+    # analytical = lambda x: A * f0 / (f0 + (A - f0) * np.exp(- alpha * A * x))
+    
+    
     f0 = 1
     euler = lambda x: - np.exp(x)
-    trial = lambda x, params: f0 - x * (np.log((x + ff(x, params))) - x)
+    trial = lambda x, params: f0 - x * ff(x, params)
     ode = lambda x, params: - np.exp(trial(x, params))
+    analytical = lambda x: - np.log(np.exp(- 1) + x)
     
     nn.set_trial(trial)
     nn.set_ode(ode)
-    eta = lambda x: 0.01
-    nn.train(x, 5000, learning_rate=eta)
+    eta = lambda x: 1e-3
+    nn.train(x, 1000, learning_rate=eta)
     
     nn_sol = nn.compute_solution(x)
     
     plt.figure(1)
     plt.plot(x, nn_sol, label="NeuralODE")
     plt.plot(x, forward_euler(x, f0, euler), label="Forward Euler")
-    plt.plot(x, - np.log(np.exp(-1) + x), label="Analytic")
+    plt.plot(x, analytical(x), label="Analytic")
     plt.xlabel(r"$x$")
     plt.ylabel(r"$f(x)$")
     plt.legend()
