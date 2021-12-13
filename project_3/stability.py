f5a011c6afc20efe31b2b39971fe12e01f6c7323
João Inácio <jgci2000@hotmail.com>
2021-12-13 17:12:44 +0100

    added stability plot for fd method

---
+++ project_3/stability.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n = 1000
+dx = np.linspace(0, 20, n)
+dt = np.linspace(0, 100, n)
+dX, dT = np.meshgrid(dx, dt)
+
+stability = 2 * dT / dX**2
+
+region = np.zeros_like(stability)
+
+for i in range(n):
+    for j in range(n):
+        if stability[i, j] < 1:
+            region[i, j] = 1
+
+plt.figure("Stability Region")
+plt.title("Stability region for finite differences")
+plt.contourf(dX, dT, region)
+plt.xlabel("$\Delta x$")
+plt.ylabel("$\Delta t$")
+
+plt.savefig("figs/stability_region.pdf")
